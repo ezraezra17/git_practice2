5cec5ae540ade0095eac06b6717de8ed71782c02
eshazine <eshazine@gmail.com>
2022-04-09 10:37:26 +0300

    My ultimate goal in this course

---
+++ src/us100/TestCase001.java
@@ -0,0 +1,7 @@
+package us100;
+
+public class TestCase001 {
+    public static void main(String[] args) {
+        System.out.println("I am one hundred percent sure that I will be a Full Stack Sdet");
+    }
+}
