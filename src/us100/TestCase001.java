047cb0964b4865aec2a43d8d15f6dcfc69f0e013
eshazine <eshazine@gmail.com>
2022-04-09 10:38:27 +0300

    Be Positive

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -3,5 +3,9 @@ package us100;
 public class TestCase001 {
     public static void main(String[] args) {
         System.out.println("I am one hundred percent sure that I will be a Full Stack Sdet");
+
+        System.out.println("Everthing will pan put in the end");
+
+
     }
 }
